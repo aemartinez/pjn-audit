a0fc9f4528d2b2bbdb9a4be67f05b454ce99ca07
Agustin Martinez <aemartinez@dc.uba.ar>
2020-02-27 17:35:43 -0300

    running version with captcha, one page

--- cijscraper.py
+++ cijscraper.py
@@ -10,8 +10,11 @@ import requests
 import pyquery as pq
 import collections
 import json
+import shutil
+from PIL import Image
 
 CIJ_URL = "https://www.cij.gov.ar/sorteos"
+CIJ_CAPTCHA_URL = " https://www.cij.gov.ar/lib/securimage/securimage_show.php"
 CAMPOS_MAP = {'Fecha de Asignación': 'fecha', 'Expediente':'cod', 'Tipo':'tipo', 'Motivo Asignación':'motivo', 'Dependencia Asignada':'dependencia', 'Denunciantes':'denunciante', 'Denunciados':'denunciados', 'Delitos':'delitos', 'Origen':'origen'}
 DatosSorteo = dict
 
@@ -22,11 +25,56 @@ class CIJScraper:
         self._base_url = url
 
     def scrap_to_json(self, filename):
-        """Scrapea la URL y almacena la informacion en filename.csv"""
+        """Scrapea la URL y almacena la informacion en filename.json"""
 
         lista_sorteos = list()
-        resp = requests.get(self._base_url)
+        s = requests.Session() 
+        resp = s.get(CIJ_CAPTCHA_URL, stream=True)
+        cookies = resp.cookies
+        with open('captcha.png', 'wb') as out_file:
+            shutil.copyfileobj(resp.raw, out_file)
+        del resp
+
+        image = Image.open('captcha.png')
+        image.show()
+
+        captcha_code = input("Escribir captcha: ")
+
+        # POST REQUEST EXAMPLE
+        # Request URL: https://www.cij.gov.ar/sorteos
+        # Request Method: POST
+        # 
+        # fecha_exacta_juzgados: 27/02/2020
+        # selector: rango
+        # fecha_juzgados_desde: 2019-02-01
+        # fecha_juzgados_desde_aux: 01/02/2019
+        # fecha_juzgados_hasta: 2020-02-10
+        # fecha_juzgados_hasta_aux: 10/02/2020
+        # oficina: 
+        # nombre[]: 
+        # paginaS1: 0
+        # paginaS2: 0
+        # origenPaginado: S1
+        # paginado: 0
+        # captcha_code: sC5E
+        # btn: Buscar
+        post_data = {
+            'selector': 'rango',
+            'fecha_juzgados_desde': '2019-02-01',
+            'fecha_juzgados_desde_aux': '01/02/2019',
+            'fecha_juzgados_hasta': '2020-02-10',
+            'fecha_juzgados_hasta_aux': '10/02/2020',
+            'paginaS1': '0',
+            'paginaS2': '0',
+            'origenPaginado': 'S1',
+            'paginado': '0',
+            'captcha_code': captcha_code,
+            'btn': 'Buscar',
+        }
+        resp = s.post(self._base_url, data = post_data, cookies=cookies)
         d = pq.PyQuery(resp.text)
+        with open('web.html', 'w') as fp:
+            fp.write(d.html())
         results = d('#sorteos-resultado').children(".result")
         for res in results.items():
             datos = res("ul li")
@@ -60,12 +108,6 @@ class CIJScraper:
         texto = texto.replace(' Ver menos', '')
         texto = texto.replace(' Ver todos', '')
         return texto
-
-
-
-"""Informacion de un sorteo de causa judicial"""
-
-
 
 def main():
     scraper = CIJScraper()
